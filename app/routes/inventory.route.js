4c6ee055005d139d9f9ebab74b120e9a4ef495eb
ShruthiGarlapati <47714415+ShruthiGarlapati@users.noreply.github.com>
2020-07-02 21:06:16 +0530

    Update inventory.route.js

--- app/routes/inventory.route.js
+++ app/routes/inventory.route.js
@@ -1,28 +1,33 @@
-module.exports = app => {
-    const inventory = require("../controllers/inventory.controller.js");
+module.exports= app => {
+    const tutorials = require("../controllers/inventory.controller.js");
+    const custs=require("../controllers/inventory.controller.js");
+   // const cust= require("../controllers/inventory.controller");
   
     var router = require("express").Router();
   
     // Create a new Tutorial
-    router.post("/", inventory.create);
+    router.post("/", tutorials.createProduct);
+    router.post("/customer", tutorials.createCustomer );
   
     // Retrieve all inventory
-    router.get("/", inventory.findAll);
-  
+    router.get("/", tutorials.findAllProds);
+    router.get("/customer", tutorials.findAllSales);
+    
     // Retrieve all published inventory
     //router.get("/published", inventory.findAllPublished);
   
     // Retrieve a single Tutorial with id
-    router.get("/:id", inventory.findOne);
+    router.get("/:Productname", tutorials.findOne);
+    router.get("/customer:Currentdate", tutorials.findDate);
   
     // Update a Tutorial with id
-    router.put("/:id", inventory.update);
+    //router.put("/:id", tutorials.update);
   
     // Delete a Tutorial with id
-    router.delete("/:id", inventory.delete);
+    router.delete("/:id", tutorials.delete);
   
     // Create a new Tutorial
-    router.delete("/", inventory.deleteAll);
+    router.delete("/", tutorials.deleteAll);
   
-    app.use('/api/inventory', router);
+    app.use('/api/tutorials', router);
   };
